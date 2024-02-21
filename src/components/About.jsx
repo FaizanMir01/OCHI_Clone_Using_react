e85eecbe852f4d704ebd276da25321733be093d8
MirFaizan01 <faizanmeer322@gmail.com>
2024-02-21 23:01:27 +0530

    completed the few parsts of about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 
 const About = () => {
   return (
-    <div className='w-full bg-[#cdea68] '>
-        <div className="para1">
-            <h1 className="font-8xl">Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.
-</h1>
+    <div className='w-full bg-[#cdea68] rounded-t-[3rem] '>
+        <div className="para1 p-20">
+            <h1 className="text-[3.7vw] leading-[4vw] tracking-tight">Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell produts, explain complex ideas, and hire great people.</h1>
+        </div>
+        <div className="w-full mid border-t-[1px] border-[#3b3b3ba4] ">
+          <div className='w-1/2 p-10 '>
+            <h1 className="text-[2vw] ">Our Approach</h1>
+            <button className="px-6 py-4 bg-zinc-800 text-zinc-100 flex items-center gap-8 rounded-full text-2xl">Read More 
+            <div className="w-3 h-3 bg-zinc-200 rounded-full"></div></button>
+          </div>
+          <div className="w-1"></div>
+          
         </div>
     </div>
   )
